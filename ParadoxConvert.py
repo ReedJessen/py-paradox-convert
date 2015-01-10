8763dc91dc74cd35b5aa2316878ee4a672487002
Peter Dowdy <peter.dowdy@gmail.com>
2015-01-10 09:36:07 -0800

    Added tokenization step to driver.

--- ParadoxConvert.py
+++ ParadoxConvert.py
@@ -1,11 +1,12 @@
 from deserializer import deserialize
 from serializer import serialize
+from tokenizer import tokenize
 
 __author__ = 'peter'
 
 
 def deserialize_eu4_text_to_object(text):
-    return deserialize(text)
+    return deserialize(tokenize(text))
 
 def serialize_object_to_eu4_file(node):
     return serialize(node)
